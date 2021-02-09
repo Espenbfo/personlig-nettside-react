5d7cd5ac3ee56edd8ef100b7135f335973efe8ff
Espen Boman Fosseide <espenbfo@stud.ntnu.no>
2021-02-09 22:39:31 +0100

    Remove react-router-dom from App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import './App.css';
-import { Switch, Route, BrowserRouter as Router, Link } from "react-router-dom";
+//import { Switch, Route, BrowserRouter as Router, Link } from "react-router-dom";
 
 import IndexPage from "./pages/IndexPage"
 
 function App() {
   return (
-      (<Router basename={process.env.PUBLIC_URL}>
-        <Switch>
-          <Route exact path={"/"}>
-            <IndexPage></IndexPage>
-          </Route>
-        </Switch>
-      </Router>)
+      <IndexPage></IndexPage>
+      //(<Router basename={process.env.PUBLIC_URL}>
+       // <Switch>
+        //  <Route exact path={"/"}>
+        //    <IndexPage></IndexPage>
+         // </Route>
+        //</Switch>
+      //</Router>)
   );
 }
 
